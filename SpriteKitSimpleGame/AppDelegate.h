060193dcf71a37d0a3b93cb346326b56e5d3a322
Jiyeon <jiyeon@Jiyeon-ui-MacBook-Air.local>
2013-10-11 16:59:39 +0900

    Initial Commit

---
+++ SpriteKitSimpleGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitSimpleGame
+//
+//  Created by Jiyeon on 13. 10. 11..
+//  Copyright (c) 2013년 Jiyeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKitSimpleGame/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SpriteKitSimpleGame
+//
+
+//  Copyright (c) 2013년 Jiyeon. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
